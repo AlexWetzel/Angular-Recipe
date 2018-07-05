253e51eaeb514090662218ba435cbc9c0bf2ad5a
AlexWetzel <wetzela@live.com>
2018-07-05 18:17:11 -0400

    Improved component file structure

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
-import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.
     AppComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
+    RecipeComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent
