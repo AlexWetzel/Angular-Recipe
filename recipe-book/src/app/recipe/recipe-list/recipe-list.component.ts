268c9eeffe8d30a1aa45aadf9ba9f43b2b0cbb9f
AlexWetzel <wetzela@live.com>
2018-07-05 19:04:04 -0400

    Recipe list prototype in progress

--- recipe-book/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test',
+     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382539856907.jpeg')
+  ];
 
   constructor() { }
 
