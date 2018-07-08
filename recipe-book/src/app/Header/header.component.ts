e32455525a0916f3fc6c976b8affeb49a4a60a08
AlexWetzel <wetzela@live.com>
2018-07-08 18:36:30 -0400

    Added pseudo-routing

--- recipe-book/src/app/Header/header.component.ts
+++ recipe-book/src/app/Header/header.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+    @Output() router = new EventEmitter();
+    page = 'recipe';
+
+    ngOnInit() {
+        this.changePage(this.page);
+    }
+
+    changePage(page) {
+        this.router.emit(page);
+    }
+
 
 }
--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  displayComponent = '';
+
+  changePage(page) {
+    this.displayComponent = page;
+  }
+
 }
