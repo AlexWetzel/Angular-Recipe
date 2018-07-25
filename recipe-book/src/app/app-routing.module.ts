c7f55da469eb2cc8cf96e64e2bb617c5abc257fe
AlexWetzel <wetzela@live.com>
2018-07-25 17:12:05 -0400

    Added edit component

--- recipe-book/src/app/app-routing.module.ts
+++ recipe-book/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
   { path: 'recipe', component: RecipeComponent, children: [
     { path: '', component: RecipeStartComponent },
-    { path: ':id', component: RecipeDetailComponent }
+    { path: 'new', component: RecipeEditComponent },
+    { path: ':id', component: RecipeDetailComponent },  
+    { path: ':id/edit', component: RecipeEditComponent }
   ] },
   { path: 'shoppinglist', component: ShoppingListComponent }
 ];
